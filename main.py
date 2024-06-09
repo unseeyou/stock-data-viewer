e2bd29ec8ea873df75a0893329be87fd94513f34
unseeyou <siqi.chen111@gmail.com>
2024-06-09 22:41:37 +1000

    finished!!!

--- main.py
+++ main.py
@@ -2,20 +2,44 @@ from stockdex import TickerFactory
 import datetime
 import customtkinter as ctk
 
-ticker = TickerFactory(ticker="BHP.AX", data_source="yahoo_api").ticker
-data = ticker.price(range='200y', dataGranularity='1d').to_dict()
 
-timestamps: dict = data["timestamp"]
-prices: dict = data["close"]
+def on_submit():
+    year = "".join([i for i in year_entry.get() if i.isnumeric()])
+    stock = stock_entry.get().upper()
+    if year and stock:
+        ticker = TickerFactory(ticker=stock, data_source="yahoo_api").ticker
+        data = ticker.price(range='200y', dataGranularity='1d').to_dict()
 
-key_dates = {}
+        timestamps: dict = data["timestamp"]
+        prices: dict = data["close"]
 
-for timestamp, price in zip(timestamps, prices):
-    if "-06-30" in str(timestamps[timestamp]):
-        key_dates[timestamps[timestamp]] = prices[price]
+        key_dates = {}
 
-print(key_dates[datetime.datetime(2020, 6, 30)])
+        for timestamp, price in zip(timestamps, prices):
+            if "-06-30" in str(timestamps[timestamp]):
+                key_dates[str(timestamps[timestamp].year)] = prices[price]
+        try:
+            output.configure(text=f"The price of {stock} at June 30th of {year} was: {key_dates[year]}")
+        except KeyError:
+            output.configure(text="There is no data for that year!")
+    else:
+        output.configure(text="Please fill in all the details before searching!")
 
 
 root = ctk.CTk()
+root.title("Stock Viewer")
+root.geometry("500x200")
+
+year_entry = ctk.CTkEntry(root, corner_radius=5, placeholder_text="year", width=60)
+year_entry.pack()
+
+stock_entry = ctk.CTkEntry(root, corner_radius=5, placeholder_text="stock", width=90)
+stock_entry.pack()
+
+submit_button = ctk.CTkButton(root, text="Search", command=lambda: on_submit())
+submit_button.pack()
+
+output = ctk.CTkLabel(root, text="Click the search button after filling in the details above!")
+output.pack()
+
 root.mainloop()
