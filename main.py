da2a14ef49593f8f11c8c726a726d7cf2527eb23
unseeyou <siqi.chen111@gmail.com>
2024-06-15 17:02:01 +1000

    make sure that the program terminates when window is closed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from stockdex import TickerFactory
-import datetime
+import datetime, sys
 import customtkinter as ctk
 import FreeSimpleGUI as sg
 
 
 def on_submit():
+    file = sg.popup_get_file("Choose a file")
     year = "".join([i for i in year_entry.get() if i.isnumeric()])
     stock = stock_entry.get().upper()
     if year and stock:
@@ -21,24 +22,42 @@ def on_submit():
                 if "-06-30" in str(timestamps[timestamp]):
                     key_dates[str(timestamps[timestamp].year)] = prices[price]
 
-        except BaseException:
-            output.configure(text="\nOops! Something went wrong!\nPlease check your internet connection and try again.")
+        except Exception as err:
+            sg.popup(err, title="Oops! Error Encountered!")
         try:
             output.configure(text=f"\nThe price of {stock} at June 30th of {year} was: {round(key_dates[year], 2)}")
         except KeyError:
             output.configure(text="\nThere is no data for that year!")
     else:
         output.configure(text="\nPlease fill in all the details before searching!")
+    return 0
+
+
+def on_closing():
+    if sg.popup_yes_no("Quit", "Do you want to quit?", background_color="red", no_titlebar=True, button_color="darkred").lower() == "yes":
+        root.destroy()
+        sys.exit()
 
 
 root = ctk.CTk()
 root.title("Stock Viewer")
-root.geometry("500x200")
+w = 500  # width for the Tk root
+h = 200  # height for the Tk root
+
+# get screen width and height
+ws = root.winfo_screenwidth()  # width of the screen
+hs = root.winfo_screenheight()  # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+# set the dimensions of the screen
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 title = ctk.CTkLabel(root, text="\nStock Price Checker\n")
 title.pack()
-
-spreadsheet = tk.
 
 year_entry = ctk.CTkEntry(root, corner_radius=5, placeholder_text="year", width=60)
 year_entry.pack()
@@ -52,4 +71,7 @@ submit_button.pack()
 output = ctk.CTkLabel(root, text="\nClick the search button after filling in the details above!")
 output.pack()
 
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
 root.mainloop()
+sys.exit(222222)
